259cc549e46810e88fd94b12da701c3b38abd92a
antonzorin <anton256@gmail.com>
2017-06-12 22:57:13 +0300

    Centered to point text added

--- app/src/main/java/com/az/drawingtest/Painting.java
+++ app/src/main/java/com/az/drawingtest/Painting.java
@@ -3,6 +3,7 @@ package com.az.drawingtest;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
@@ -22,6 +23,8 @@ public class Painting extends View {
     Paint circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint dotPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    String testText = "center";
+    Rect textRect;
 
     public Painting(Context context) {
         super(context);
@@ -39,10 +42,12 @@ public class Painting extends View {
     }
 
     void init(Context context) {
+        textRect = new Rect();
         circlePaint.setColor(context.getResources().getColor(android.R.color.holo_purple));
         dotPaint.setColor(context.getResources().getColor(android.R.color.holo_red_light));
-       textPaint.setColor(context.getResources().getColor(android.R.color.black));
-
+        textPaint.setColor(context.getResources().getColor(android.R.color.black));
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(22);
     }
 
     @Override
@@ -58,9 +63,11 @@ public class Painting extends View {
         double thetaRad = Math.PI * (angle - 90) / 180F;
         int radiusWithOffset = radius - 20;
 
-        double newPx = cX + radiusWithOffset * cos(thetaRad);
-        double newPy = cY + radiusWithOffset * sin(thetaRad);
+        double cPx = cX + radiusWithOffset * cos(thetaRad);
+        double cPy = cY + radiusWithOffset * sin(thetaRad);
+        canvas.drawCircle((int) cPx, (int) cPy, 15, dotPaint);
 
-        canvas.drawCircle((int) newPx, (int) newPy, 15, dotPaint);
+        textPaint.getTextBounds(testText, 0, testText.length(), textRect);
+        canvas.drawText(testText, (int) cPx, (int) cPy - textRect.centerY(), textPaint);
     }
 }
